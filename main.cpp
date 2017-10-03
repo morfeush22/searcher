2d697275d17d36aee74270cc55158581d96857b9
morfeush22 <patro.kacper@gmail.com>
2017-10-03 20:40:13 +1100

    main function refactored

--- main.cpp
+++ main.cpp
@@ -8,26 +8,34 @@
 #include "Searcher.h"
 #include "Printer.h"
 #include "PrintSearchTask.h"
+#include "SearcherFactory.h"
 
 namespace fs = std::experimental::filesystem;
 using namespace std;
 
+void SearchAndPrint(const std::string &path, const std::string &str) {
+	fs::directory_options opts = fs::directory_options::follow_directory_symlink | fs::directory_options::skip_permission_denied;
+	std::unique_ptr<SearcherFactory> factory = std::make_unique<SearcherFactory>();
+	std::unique_ptr<Printer> printer = std::make_unique<Printer>();
+	std::queue<std::future<void>> queue;
 
-int main(int argv, char *argc[]) {
-	std::queue<std::future<void>> q;
-	Printer printer;
-	fs::directory_options opt = fs::directory_options::follow_directory_symlink | fs::directory_options::skip_permission_denied;
-	for(auto& p: fs::recursive_directory_iterator("a", opt)) {
+	for(auto &p: fs::recursive_directory_iterator(path, opts)) {
 		if (fs::is_regular_file(p)) {
-			PrintSearchTask pst {printer, Searcher{p.path(), "dupasraka"}, p.path()};
+			std::unique_ptr<Searcher> searcher = factory->Create(p.path(), str);
+			PrintSearchTask pst {printer, std::move(searcher)};
 			auto f = std::async(std::move(pst));
-			q.push(std::move(f));
+			queue.push(std::move(f));
 		}
 	}
-	while(!q.empty()) {
-		auto f = std::move(q.front());
-		q.pop();
-		f.get();
+
+	while(!queue.empty()) {
+		auto task = std::move(queue.front());
+		queue.pop();
+		task.get();
 	}
+}
+
+int main(int argv, char *argc[]) {
+	SearchAndPrint("a", "dupasraka");
 	return 0;
 }
