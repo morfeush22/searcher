38c30e8dbc243233fd3e79bf9c069c7ba0337ea5
morfeush22 <patro.kacper@gmail.com>
2017-10-04 02:37:17 +1100

    main taking args

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <utility>
 #include <future>
 #include <queue>
-#include <type_traits>
+#include <vector>
 
 #include "Searcher.h"
 #include "Printer.h"
@@ -13,29 +13,78 @@
 namespace fs = std::experimental::filesystem;
 using namespace std;
 
-void SearchAndPrint(const std::string &path, const std::string &str) {
-	fs::directory_options opts = fs::directory_options::follow_directory_symlink | fs::directory_options::skip_permission_denied;
-	std::unique_ptr<SearcherFactory> factory = std::make_unique<SearcherFactory>();
-	std::unique_ptr<Printer> printer = std::make_unique<Printer>();
-	std::queue<std::future<void>> queue;
 
-	for(auto &p: fs::recursive_directory_iterator(path, opts)) {
-		if (fs::is_regular_file(p)) {
-			std::unique_ptr<Searcher> searcher = factory->Create(p.path(), str);
-			PrintSearchTask pst {printer, std::move(searcher)};
-			auto f = std::async(std::move(pst));
-			queue.push(std::move(f));
-		}
+void ShowUsage() {
+    std::cerr << "Usage: searcher [OPTION]... SOURCE STRING\n"
+              << "\n"
+              << "\t-h, --help\t\tShow this help message\n"
+              << std::endl;
+}
+
+template<typename Factory, typename Printer, typename Path, typename String>
+decltype(auto) CreateTask(const Factory &factory, const Printer &printer, const Path &path, const String &str) {
+	auto searcher = factory->Create(path, str);
+	PrintSearchTask pst {printer, std::move(searcher)};
+	return pst;
+}
+
+void SearchAndPrint(const std::string &path, const std::string &str) {
+	if (!fs::exists(path)) {
+		std::cerr << "file do not exist... " << path << "\n";
+		return;
 	}
 
-	while(!queue.empty()) {
-		auto task = std::move(queue.front());
-		queue.pop();
-		task.get();
+	std::unique_ptr<SearcherFactory> factory = std::make_unique<SearcherFactory>();
+	std::unique_ptr<Printer> printer = std::make_unique<Printer>();
+
+	if (fs::is_regular_file(path)) {
+		auto pst = CreateTask(factory, printer, path, str);
+		pst();
+	}
+	else if (fs::is_directory(path)) {
+		fs::directory_options opts = fs::directory_options::follow_directory_symlink | fs::directory_options::skip_permission_denied;
+		std::queue<std::future<void>> queue;
+
+		for(auto &p: fs::recursive_directory_iterator(path, opts)) {
+			if (fs::is_regular_file(p)) {
+				auto pst = CreateTask(factory, printer, p.path(), str);
+				auto fut = std::async(std::move(pst));
+				queue.push(std::move(fut));
+			}
+		}
+
+		while(!queue.empty()) {
+			auto task = std::move(queue.front());
+			queue.pop();
+			task.get();
+		}
+	}
+	else {
+		std::cerr << "wrong file... " << path << "\n";
+		return;
 	}
 }
 
-int main(int argv, char *argc[]) {
-	SearchAndPrint("a", "dupasraka");
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+    	ShowUsage();
+        return 1;
+    }
+
+    std::vector<std::string> args;
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if ((arg == "-h") || (arg == "--help")) {
+        	ShowUsage();
+            return 0;
+        }
+        else {
+            args.push_back(argv[i]);
+        }
+    }
+
+	SearchAndPrint(args[0], args[1]);
+
 	return 0;
 }
